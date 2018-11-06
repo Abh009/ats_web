264644b9fc65fd5ec3d652835458bd9f39d96f93
Ubuntu <ubuntu@ip-172-31-42-171.us-east-2.compute.internal>
2018-11-06 19:34:52 +0000

    pull

--- signup_faculties.php
+++ signup_faculties.php
@@ -12,13 +12,13 @@
 
     // get data sent via POST
     $regno = $_POST['regno'];
-    $password = $_POST['password'];
+    $passwrd = $_POST['password'];
 
     // authenticate
-    $sql = "SELECT * FROM faculties WHERE regno = '$regno' and password = password($password)";
+    $sql = "SELECT * FROM faculties WHERE regno = '$regno' and password = password('$passwrd')";
     $result = $con->query( $sql );
 
-    if ( $result->num_rows <= 0 ){
+     if ( $result->num_rows <= 0 ){
         echo json_encode( array( 'status'=>0, 'text'=>'Incorrect faculty ID or password') );
         exit();
     }
