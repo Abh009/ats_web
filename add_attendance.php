bbeefbe3aa0750fc5e7058a1771cae85ea0290fa
Abhinav KM <abhinavkm184@gmail.com>
2018-11-15 22:17:11 +0530

    add attendance adding

---
+++ add_attendance.php
@@ -0,0 +1,15 @@
+<?php
+    
+    require('db.php');
+    $con = connect_db();
+
+    if( !isset( $_POST['faculty_id'], $_POST['absentees'])){
+        echo json_encode( array( 'status'=>0, 'text'=>'Invalid Request'));
+        exit();
+    }
+
+    $faculty_id = $_POST['faculty_id'];
+    $absentees_list = json_decode ( $_POST['absentees'] );
+
+    echo $absentees_list;
+?>
