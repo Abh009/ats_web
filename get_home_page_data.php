18450cb526a380c57ad5afe0e8cb788438ff2d31
Abhinav KM <abhinavkm184@gmail.com>
2018-11-18 15:39:41 +0530

    b

--- get_home_page_data.php
+++ get_home_page_data.php
@@ -1,8 +1,9 @@
 <?php
+    require("get_students_with_shortage.php");
+    require("get_faculty_timetable.php");
+    
     $faculty_timetable =  get_faculty_timetable();
-    echo "1";
     $students_with_shortage = get_students_with_shortage();
-    echo "2";
 
     $faculty_timetable['students_with_shortage'] = $students_with_shortage;
     echo json_encode( $faculty_timetable );
