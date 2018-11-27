aa22f264ca14ed615943b54700a5c75969d2cbe4
Abhinav KM <abhinavkm184@gmail.com>
2018-11-28 01:55:18 +0530

    pasiasjias

--- add_assignment.php
+++ add_assignment.php
@@ -29,7 +29,7 @@
     echo $date_of_subm;
 
     // create a notification
-    $sql = "INSERT INTO assignment VALUES( '$faculty_id', '$branch', '$sem', $batch, '$subject', '$topic', $date_of_subm )";
+    $sql = "INSERT INTO assignment VALUES( '$faculty_id', '$branch', '$sem', $batch, '$subject', '$topic', '$date_of_subm' )";
     $result = $con->query( $sql );
 
     echo json_encode( array( 'status'=>1, 'text'=>'Assignment added'));
